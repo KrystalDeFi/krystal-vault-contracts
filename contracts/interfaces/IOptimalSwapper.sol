a91cea50325491cf7d5ef26f9d44b6335fd9dddd
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2025-02-25 16:34:35 +0700

    implement optimal swapper reader

--- contracts/interfaces/IOptimalSwapper.sol
+++ contracts/interfaces/IOptimalSwapper.sol
@@ -11,4 +11,13 @@ interface IOptimalSwapper {
     int24 tickUpper,
     bytes calldata data
   ) external;
+
+  function getOptimalSwapAmounts(
+    address pool,
+    uint256 amount0Desired,
+    uint256 amount1Desired,
+    int24 tickLower,
+    int24 tickUpper,
+    bytes calldata data
+  ) external view returns (uint256 amount0, uint256 amount1);
 }
